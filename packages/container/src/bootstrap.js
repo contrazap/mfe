cbc8be3139d9f3ba764631d3cbab910a9f307872
contrazap <contrazap@gmail.com>
2025-07-23 15:08:45 +0530

    Added test log to trigger github action

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Render react app
+console.log("Render react app");
 ReactDOM.render(<App />, document.querySelector("#root"));
