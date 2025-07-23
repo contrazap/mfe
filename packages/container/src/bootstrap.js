21873f387a9a8b86635c48f03e700b7ec6db498f
contrazap <contrazap@gmail.com>
2025-07-23 15:15:19 +0530

    modified container.yml file to test github action

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-console.log("Render react app");
 ReactDOM.render(<App />, document.querySelector("#root"));
